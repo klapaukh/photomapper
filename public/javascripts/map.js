057a4475565c9021d3a44984a194c060b87fd979
Roma Klapaukh <rklapaukh@hotmail.com>
2016-05-18 10:02:16 +1200

    Getting icons to display

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -8,7 +8,18 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-var markers = L.markerClusterGroup();
+
+//Extend the Default marker class
+var MyIcon = L.Icon.Default.extend({
+   options: {
+   	    iconUrl: '/images/marker-icon.png',
+        iconRetinaUrl: '/images/marker-icon-2x.png',
+        shadowUrl: '/images/marker-shadow.png',
+        shadowRetinaUrl: '/images/marker-shadow.png'
+   }
+});
+
+var myIcon = new MyIcon();
 
 // All of the data comes from the servers photos endpoint
 $.get("/photos",function(result) { 
@@ -17,6 +28,6 @@ $.get("/photos",function(result) {
   var markers = L.markerClusterGroup();
 
   result.forEach(function(photo){
-    L.marker([photo.lat,photo.lon]).addTo(mymap);
+    L.marker([photo.lat,photo.lon],{icon: myIcon}).addTo(mymap);
   });
 })
